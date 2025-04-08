55776e402ef694d1da2727d184f5f920a737eeb3
Zoey Hamm <339622581@GoTVDSB.ca>
2025-04-08 14:15:00 -0400

    Add project files.

---
+++ Die Class.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monogame_4___The_Die_Class
+{
+    public class Die
+    {
+        private int _roll;
+        private Random _generator;
+
+        public Die()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 7);
+        }
+
+
+        public int Roll
+        {
+            get { return _roll; }
+            //set 
+            //{ 
+            //    _roll = value; 
+            //}
+        }
+
+        public int RollDie()
+        {
+            _roll = _generator.Next(1, 7);
+            return _roll;
+        }
+
+        public void DrawRoll()
+        {
+            if (_roll == 1)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|   |");
+                Console.WriteLine("| o |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 2)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o  |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("|  o|");
+                Console.WriteLine("-----");
+            }
+
+            else if (_roll == 3)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o  |");
+                Console.WriteLine("| o |");
+                Console.WriteLine("|  o|");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 4)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("|   |");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 5)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("| o |");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 6)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("-----");
+            }
+            else
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|   |");
+                Console.WriteLine($"| {_roll} |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("-----");
+            }
+
+
+
+
+        }
+
+        public override string ToString()
+        {
+            return _roll.ToString();
+        }
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Monogame_4___The_Die_Class.Game1();
+game.Run();
